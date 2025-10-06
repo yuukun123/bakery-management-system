5f41f6939e335a4650e22cd105ec19a6ddc10ba5
Yuu <152776955+yuukun123@users.noreply.github.com>
2025-10-06 17:37:27 +0700

    update folder

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt5 import QtWidgets, QtCore
+
+from src.windows.employee_window.employee_window_manage import open_employee_main_window
+
+# from src.views.login_regis_view.login_regis import Login_and_Register_Window
+
+if __name__ == "__main__":
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+
+    app = QtWidgets.QApplication(sys.argv)
+    # open_login_window()
+    window = open_employee_main_window()
+    sys.exit(app.exec_())
--- src/views/employee_main_view/employee_main_view.py
+++ src/views/employee_main_view/employee_main_view.py
@@ -2,74 +2,46 @@ from PyQt5 import uic
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow, QMessageBox, QApplication
 
-from src.services.query_data.query_data import QueryData
-from src.views.main_view.practice_view import PracticeWindow
-from src.views.main_view.topic_view import TopicWindow
-from src.views.moveable_window import MoveableWindow
-from src.views.main_view.practice_opt_view import topic_practice
+# from src.services.query_data.query_data import QueryData
 from src.controllers.buttonController import buttonController
-from src.utils.username_ui import set_user_info
+# from src.utils.username_ui import set_user_info
 from src.views.moveable_window import MoveableWindow
-class MainWindow(QMainWindow, MoveableWindow):
-    def __init__(self, username):
+from resources import resources_rc
+
+class EmployeeMainWindow(QMainWindow, MoveableWindow):
+    def __init__(self):
         # self.username = username
         super().__init__()
-        uic.loadUi("UI/forms/main_screen.ui", self)
+        uic.loadUi("UI/forms/employee/employee_main_screen.ui", self)
         MoveableWindow.__init__(self)
 
-        self.go_back.hide()
+        # self.go_back.hide()
 
         # Thêm frameless + trong suốt
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setWindowOpacity(1.0)
 
-        self.query_data = QueryData()
-        self._user_context = None
-        self.load_user_context(username)
-
-        if not self._user_context:
-            QMessageBox.critical(self, "Lỗi nghiêm trọng", f"Không thể tìm thấy dữ liệu cho người dùng '{username}'.")
-            return
-
-        set_user_info(self.username_label, username)
+        # self.query_data = QueryData()
+        # self._user_context = None
+        # self.load_user_context(username)
+        #
+        # if not self._user_context:
+        #     QMessageBox.critical(self, "Lỗi nghiêm trọng", f"Không thể tìm thấy dữ liệu cho người dùng '{username}'.")
+        #     return
+        #
+        # set_user_info(self.username_label, username)
 
         self.buttonController = buttonController(self)
         self.closeBtn.clicked.connect(buttonController.handle_close)
         self.hideBtn.clicked.connect(self.buttonController.handle_hidden)
-        self.logout.clicked.connect(self.buttonController.handle_logout)
+        # self.logout.clicked.connect(self.buttonController.handle_logout)
+        #
+        # self.vocab.clicked.connect(self.handle_topic_window_click)
+        # self.practice.clicked.connect(self.handle_practice_window_click)
+        # print("DEBUG: vocab button connected")
 
-        self.vocab.clicked.connect(self.handle_topic_window_click)
-        self.practice.clicked.connect(self.handle_practice_window_click)
-        print("DEBUG: vocab button connected")
-
-    def load_user_context(self, username):
-        print(f"DEBUG: Đang tải user context cho username: {username}")
-        self._user_context = self.query_data.get_user_by_username(username)
-        print(f"DEBUG: User context đã tải: {self._user_context}")
-
-    def handle_topic_window_click(self):
-        print("DEBUG: start open_topic_window")
-        if not self._user_context:
-            # Sử dụng self._user_context để lấy username cho thông báo lỗi
-            user_name_for_msg = self.username # Hoặc một giá trị mặc định
-            QMessageBox.critical(self, "Lỗi nghiêm trọng", f"Không thể tìm thấy dữ liệu cho người dùng '{user_name_for_msg}'.")
-            return
-        try:
-            self.hide()  # ẩn ngay lập tức
-            current_username = self._user_context.get('user_name')
-            self.topic_window = TopicWindow(parent=self, username=current_username, main_window=self)
-            self.topic_window.topic_controller.setup_for_user(self._user_context)
-            print("DEBUG: topic_window created", self.topic_window)
-            self.topic_window.show()
-            print("DEBUG: vocab_window show called")
-        except Exception as e:
-            print("ERROR while opening topic window:", e)
-            self.show()
-
-    def handle_practice_window_click(self):
-        print("DEBUG: start open dialog topic_for_practice")
-        dialog = topic_practice(self._user_context, self)
-        dialog.open()
-
-
+    # def load_user_context(self, username):
+    #     print(f"DEBUG: Đang tải user context cho username: {username}")
+    #     self._user_context = self.query_data.get_user_by_username(username)
+    #     print(f"DEBUG: User context đã tải: {self._user_context}")
