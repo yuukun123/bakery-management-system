96296ce0ccd3e18c762b9375bea5415db89e8c44
Yuu <152776955+yuukun123@users.noreply.github.com>
2025-10-14 08:14:02 +0700

    update folder

--- main.py
+++ main.py
@@ -11,28 +11,23 @@ if __name__ == "__main__":
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
 
-    # app = QtWidgets.QApplication(sys.argv)
-    # open_login_window()
-    # # window = open_employee_main_window()
+    app = QtWidgets.QApplication(sys.argv)
+    open_login_window()
+    # window = open_employee_main_window()
+    sys.exit(app.exec_())
+
+    # # Dữ liệu test
+    # product_data = {
+    #     "product_id": 1,
+    #     "product_name": "Avocado Croissant",
+    #     "selling_price": 100000,
+    #     "image_path": "UI/images/Croissant/Avocado_Croissant.jpg"
+    # }
+    #
+    # window = QMainWindow()
+    # card = ProductCard(product_data)
+    # window.setCentralWidget(card)
+    # # window.resize(300, 300)
+    # window.show()
+    #
     # sys.exit(app.exec_())
-
-
-
-    if __name__ == "__main__":
-        app = QApplication(sys.argv)
-
-        # Dữ liệu test
-        product_data = {
-            "product_id": 1,
-            "product_name": "Avocado Croissant",
-            "selling_price": 100000,
-            "image_path": "UI/images/Croissant/Avocado_Croissant.jpg"
-        }
-
-        window = QMainWindow()
-        card = ProductCard(product_data)
-        window.setCentralWidget(card)
-        # window.resize(300, 300)
-        window.show()
-
-        sys.exit(app.exec_())
