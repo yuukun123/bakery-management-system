030936ed74edf4dda39374e8cd996a4c64770afc
Yuu <152776955+yuukun123@users.noreply.github.com>
2025-10-20 14:13:16 +0700

    update folder

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import QtWidgets, QtCore
 
 from src.windows.login_window import open_login_window
-
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
 from src.views.employee_main_view.product_card import ProductCard  # nếu bạn lưu class trên vào file product_card.py
 
 if __name__ == "__main__":
@@ -12,22 +10,22 @@ if __name__ == "__main__":
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
 
     app = QtWidgets.QApplication(sys.argv)
-    # open_login_window()
-    # # window = open_employee_main_window()
-    # sys.exit(app.exec_())
+    open_login_window()
+    # window = open_employee_main_window()
+    sys.exit(app.exec_())
 
     # Dữ liệu test
-    product_data = {
-        "product_id": 1,
-        "product_name": "Avocado Croissant",
-        "selling_price": 100000,
-        "image_path": "UI/images/Mousse/Avocado_Mousse.jpg"
-    }
-
-    window = QMainWindow()
-    card = ProductCard(product_data)
-    window.setCentralWidget(card)
-    # window.resize(300, 300)
-    window.show()
-
-    sys.exit(app.exec_())
+    # product_data = {
+    #     "product_id": 1,
+    #     "product_name": "Strawberry Dream Croissant",
+    #     "selling_price": 100000,
+    #     "image_path": "UI/images/Mousse/Avocado_Mousse.jpg"
+    # }
+    #
+    # window = QMainWindow()
+    # card = ProductCard(product_data)
+    # window.setCentralWidget(card)
+    # # window.resize(300, 300)
+    # window.show()
+    #
+    # sys.exit(app.exec_())
